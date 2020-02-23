3e6028ba4cf0112d1eb9109fd80d23255b1502d2
yingkangfang <fangyingkang@163.com>
2020-02-23 19:45:47 +0800

    【模块】week5 【修改原因】增加dnn训练模型

--- 李宏毅机器学习-作业/week5/DNN.py
+++ 李宏毅机器学习-作业/week5/DNN.py
@@ -11,45 +11,57 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
+from keras.optimizers import SGD
+from keras.utils import to_categorical
 
-batch_size = 128
-num_classes = 10
-epochs = 20
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+def load_data():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(path='./data')
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+    return deal_data(x_train, y_train, x_test, y_test)
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(num_classes, activation='softmax'))
+def deal_data(x_train, y_train, x_test, y_test):
+    # reshape数据格式
+    x_train = x_train.reshape(60000, 28*28)
+    x_test = x_test.reshape(10000, 28*28)
 
-model.summary()
+    # 将数据格式进行转换
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
-              metrics=['accuracy'])
+    # 标准化
+    x_train /= 255
+    x_test /= 255
 
-history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+    print('trian samples:', x_train.shape[0])
+    print('test samples:', x_test.shape[0])
+
+    # convert class vectors to binary class matrices
+    y_train = to_categorical(y_train, num_classes=10)
+    y_test = to_categorical(y_test, num_classes=10)
+
+    return x_train, y_train, x_test, y_test
+
+
+def dnn():
+    model = Sequential()
+    model.add(Dense(input_shape=(784, ), units=512, activation='sigmoid'))
+    model.add(Dense(units=512, activation='sigmoid'))
+    model.add(Dense(units=10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=SGD(learning_rate=0.01),
+                  metrics=['accuracy'])
+
+    return model
+
+
+if __name__ == '__main__':
+    x_train, y_train, x_test, y_test = load_data()
+    model = dnn()
+
+    model.fit(x_train, y_train, batch_size=100, epochs=2)
+    print('Train accuracy:', model.evaluate(x_train, y_train))
+    print('Test accuracy:', model.evaluate(x_test, y_test))
+
